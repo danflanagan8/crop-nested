71b9427c487fb47369908be5a30e6c18a2ebe9f6
Dan Flanagan <dan.flanagan@manifest.com>
2018-08-22 12:40:47 -0500

    Update the field formatter (beta) so the nest and egg are configurable. Still relies on image style and corresponding crop having the same machine name.

--- crop_coordinates/src/Plugin/Field/FieldFormatter/CropCoordinatesFormatterBeta.php
+++ crop_coordinates/src/Plugin/Field/FieldFormatter/CropCoordinatesFormatterBeta.php
@@ -26,9 +26,70 @@ class CropCoordinatesFormatterBeta extends ImageFormatter {
   /**
    * {@inheritdoc}
    */
-  public function settingsSummary() {
-    $summary = ['Thumb to Featured exploder'];
-    return $summary;
+   public function settingsSummary() {
+     $summary = [];
+
+     $image_styles = image_style_options(FALSE);
+     // Unset possible 'No defined styles' option.
+     unset($image_styles['']);
+     // Styles could be lost because of enabled/disabled modules that defines
+     // their styles in code.
+     $image_style_setting = $this->getSetting('image_style');
+     if (isset($image_styles[$image_style_setting])) {
+       $summary[] = t('Nest style: @style', ['@style' => $image_styles[$image_style_setting]]);
+     }
+     else {
+       $summary[] = t('Nest style: Original image');
+     }
+     if (isset($image_styles[$this->getSetting('egg_style')])) {
+       $summary[] = t('Egg style: @style', ['@style' => $image_styles[$this->getSetting('egg_style')]]);
+     }
+     else {
+       $summary[] = t('Egg style: Original image');
+     }
+
+     $link_types = [
+       'content' => t('Linked to content'),
+       'file' => t('Linked to file'),
+     ];
+     // Display this setting only if image is linked.
+     $image_link_setting = $this->getSetting('image_link');
+     if (isset($link_types[$image_link_setting])) {
+       $summary[] = $link_types[$image_link_setting];
+     }
+
+     return $summary;
+   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'image_style' => '',
+      'egg_style' => '',
+      'image_link' => '',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $image_styles = image_style_options(FALSE);
+    $element = parent::settingsForm($form, $form_state);
+    $element['egg_style'] = [
+      '#title' => t('Egg image style'),
+      '#type' => 'select',
+      '#default_value' => $this->getSetting('egg_style'),
+      '#empty_option' => t('None (original image)'),
+      '#options' => $image_styles,
+      '#weight' => -1,
+      '#description' => t('The Egg image style should be nested within the Nest image style.'),
+    ];
+    $element['image_style']['#weight'] = -2;
+    $element['image_style']['#title'] = 'Nest image style';
+    return $element;
   }
 
   /**
@@ -40,6 +101,9 @@ class CropCoordinatesFormatterBeta extends ImageFormatter {
 
     $crop_storage = \Drupal::service('entity.manager')->getStorage('crop');
     $crop_types = $crop_storage->loadMultiple();
+    $egg = $this->getSetting('egg_style');
+    $nest = $this->getSetting('image_style');
+
     $index = 0;
     foreach($elements as &$element){
       $image = $images[$index];
@@ -55,12 +119,12 @@ class CropCoordinatesFormatterBeta extends ImageFormatter {
       }
       //calculate where the thumb crop sits within the featured crop
       //The x and y values are the CENTER of the crop.
-      //We calculate where the top-left corner of thumb sits relative to the
-      //top-left corner of featured.
-      $element['#item_attributes']['data-crop-x'][] = ($crops['thumb']['position']['x'] - 0.5 * $crops['thumb']['size']['width']) - ($crops['featured']['position']['x'] - 0.5*$crops['featured']['size']['width']);
-      $element['#item_attributes']['data-crop-y'][] = ($crops['thumb']['position']['y'] - 0.5 * $crops['thumb']['size']['height']) - ($crops['featured']['position']['y'] - 0.5*$crops['featured']['size']['height']);
-      $element['#item_attributes']['data-crop-width'][] = $crops['thumb']['size']['width'];
-      $element['#item_attributes']['data-crop-height'][] = $crops['thumb']['size']['height'];
+      //We calculate where the top-left corner of egg sits relative to the
+      //top-left corner of nest.
+      $element['#item_attributes']['data-crop-x'][] = ($crops[$egg]['position']['x'] - 0.5 * $crops[$egg]['size']['width']) - ($crops[$nest]['position']['x'] - 0.5 * $crops[$nest]['size']['width']);
+      $element['#item_attributes']['data-crop-y'][] = ($crops[$egg]['position']['y'] - 0.5 * $crops[$egg]['size']['height']) - ($crops[$nest]['position']['y'] - 0.5 * $crops[$nest]['size']['height']);
+      $element['#item_attributes']['data-crop-width'][] = $crops[$egg]['size']['width'];
+      $element['#item_attributes']['data-crop-height'][] = $crops[$egg]['size']['height'];
       $index += 1;
 
       $element['#image'] = $element;
