8f2942681c0e8f7d6c0a903c9882b2fab0b8a0cb
Dan Flanagan <dan.flanagan@manifest.com>
2018-12-24 15:45:07 -0600

    Fix some bgs with the zoom transforms.

--- crop_nested/modules/crop_nested_zoom/js/zoom.js
+++ crop_nested/modules/crop_nested_zoom/js/zoom.js
@@ -5,27 +5,20 @@
       if($cropNested.length){
         $cropNested.each(function(){
           Drupal.behaviors.cropNestedZoom.addAttributes($(this));
-          }).on('mouseover', function(){
-            $(this).addClass('zoom')
-          }).on('mouseout', function(){
-            $(this).removeClass('zoom');
+          }).on('mouseover mouseout', function(){
+            $(this).find('img').toggleClass('zoom');
           });
-        $(window).resize(function(){
-          $cropNested.each(function(){
-            Drupal.behaviors.cropNestedZoom.addAttributes($(this));
-          });
-        });
       }
     },
     addAttributes: function($cropNestedElem){
       var $img = $cropNestedElem.find('img');
       //scale determines how much the image style scaled the image
       var scale = $img.attr('width') / Number($img.data('nest-width'));
-      //browserFactor relates image actual size to image natural size.
-      var browserFactor = $img.width()/$img.attr('width');
-      //zoom relates nest crop size to egg crop size.
       var zoom = Number($img.data('nest-width')) / Number($img.data('egg-width'));
-      var translate = 'translate(-' + scale * browserFactor * $img.data('egg-x') + 'px, -' + scale * browserFactor * $img.data('egg-y') + 'px)';
+      var xTrans = zoom * $img.data('egg-x') / $img.data('nest-width') * 100;
+      var yTrans = zoom * $img.data('egg-y') / $img.data('nest-height') * 100;
+      //zoom relates nest crop size to egg crop size.
+      var translate = 'translate(-' + xTrans + '%, -' + yTrans + '%)';
       $img.css('transform', translate + 'scale(' + zoom + ')');
       $cropNestedElem.css('max-width', $img.attr('width') + 'px');
     },
